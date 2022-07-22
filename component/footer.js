effad57a6bdd975cf1f2b5e5d3cc1a06c914a3ea
Ppreedam <ppreedam@gmail.com>
2022-07-22 15:30:32 +0530

    projectcomp

---
+++ component/footer.js
@@ -0,0 +1,82 @@
+let footer=()=>{
+    return `<div id="exclusive">
+    <a href=""><img src="https://lh3.googleusercontent.com/G8v9xJof_zzUKVVhmJmHDG9WjJCBOT0nnZey7MrM8ClVQNczxJqVt-6aaiAVqK7QI8xK9WIOggVRO8aeqCPINIRpjoCkRB9YC6aoLPZz4m62gECNG4XRYZq0CZZ0lg6D6llNQTENIVX7uhBaQyWgLx1UieHIJYTVU_FIVPn9BPxEmJMIuMh2TciJ4oH5rFbKdfIWp5RccULkI8wkzkLnqfKs36nm5hJ7PTQ89w4Xc1r2vscMv4eBhQVRI0wHB8-yoI20hiREpqrOzo7EV39X7r1lyQP9Cb-5qMXFQqFALN798BgpOFxf6aQZbjlkzyxLiYPtkooseTm_pNKSLk2pL21bxWL6aBqGbf15LaAzx3R9cUXseH4hGOiWIMEl7vBLMlEmlmdeImcKgzbFg8JnX-6dT-9ioorK-29HW5pCzLWp0f2txVjEjHfzmKRbsPs4yno0AvM7TuXqlhA2t16iVBaCO-z0EpH8QAARbneH7lQUMGP5sc1An5-FMve3sb8P2bg915fxVbZOEsiSWeMalkDTRiMBmbwRRsmgzcmYT0Z6pr5oFp269j8U7IFx8iHMVBesLNrm8a_TpGGRGuI7UP-VcBU09HGIA4TgBjR98IIbS0Zi7dx_Q0mbsR5U_HvcP6Vvdcqh28oC_flb6AJPzYIS4df3sqa61KndGhlYscyJ8_DQxcrSxMebgdCk6QSBc8cr9NwXY_usckevJTGGoNwkzHK27i5VGJB7taKelEcAH1bYQ3tb3A=w1831-h213-no?authuser=0" alt=""></a>
+
+</div>
+<div id="get_email">
+    <div></div>
+    <div>
+        <p>Get E-mail Updates:</p>
+        <form action="">
+            
+            <input type="text" placeholder="E-mail Address">
+            <input type="submit" id="btn" value="SIGN UP">
+        </form>
+    </div>
+</div>
+<div id="footer">
+    <div>
+        <h2>Customer Service</h2>
+        <a href=""><p>Order Status</p></a>
+        <a href=""><p>Guest Returns</p></a>
+        <a href=""><p>Shpping & Return Policy</p></a>
+        <a href=""><p>Gift Cards</p></a>
+        <a href=""><p>FAQ</p></a>
+        <a href=""><p>Contact Us</p></a>
+    </div>
+    <div>
+        <h2>About Us</h2>
+        <a href=""><p>About Our Brand</p></a>
+        <a href=""><p>The Nordy Club</p></a>
+        <a href=""><p>Store Locator</p></a>
+        <a href=""><p>All brands</p></a>
+        <a href=""><p>Careers</p></a>
+        <a href=""><p>Get Email Updates</p></a>
+        <a href=""><p>Nordstrom Blog</p></a>
+        <a href=""><p>Nordy Podcast</p></a>
+    </div>
+    <div>
+        <h2>Nordstrom Rack & The Community</h2>
+        <a href=""><p>Corporate Social Responsibility</p></a>
+        <a href=""><p>Diversity,Inclusion & Belonging</p></a>
+        <a href=""><p>Big Brothers Big Sisters</p></a>
+        <a href=""><p>Donate Clothes</p></a>
+    </div>
+    <div>
+        <h2>Nordstrom Card</h2>
+        <a href=""><p>Apply for a Nordstorm Card</p></a>
+        <a href=""><p>Pay My Bill</p></a>
+        <a href=""><p>Manage MY Nordstorm card</p></a>
+        
+    </div>
+    <div>
+        <h2>Nordstrom, Inc.</h2>
+        <a href=""><p>Nordstorm</p></a>
+        <a href=""><p>Nordstorm Canada</p></a>
+        <a href=""><p>HauteLook</p></a>
+        <a href=""><p>Investor Relations</p></a>
+        <a href=""><p>Press Releases</p></a>
+        <a href=""><p>Nordstorm Media Network</p></a>
+    </div>
+    <div>
+        <a href=""><h2><i class="fa-thin fa-mobile-screen"></i>Download Our Apps</h2></a>
+        
+        <div id="socialicon">
+            <a href=""><i class="fa-brands fa-facebook"></i></a>
+            <a href=""><i class="fa-brands fa-twitter"></i></a>
+            <a href=""><i class="fa-brands fa-pinterest"></i></a>
+            <a href=""><i class="fa-brands fa-instagram"></i></a>
+
+        </div>
+    </div>
+
+</div>
+<div id="privacy">
+    <a href=""><p>Privacy</p></a>
+    <a href=""><p>Do Not Sell My Personal Information</p></a>
+    <a href=""><p>Terms & conditions</p></a>
+    <a href=""><p>Internet Based Ads</p></a>
+    <a href=""><p>California Chain Act</p></a>
+</div>`
+}
+export default footer
